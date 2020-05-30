7956600dac56ca7b09005b74fbec1b5b0034d370
Urian Lee <urianlee@yahoo.com>
2020-05-30 11:07:08 -0700

    kobiton urls

--- src/test/java/com/platform/project/runner/AndroidBrowserTest.java
+++ src/test/java/com/platform/project/runner/AndroidBrowserTest.java
@@ -18,12 +18,31 @@ public class AndroidBrowserTest {
 	
 	public WebDriver driver;
 	String kobitonServerUrl = "https://svatitech:84b66fb4-d842-4faf-958d-66bd97a3118b@api.kobiton.com/wd/hub";
+
+	//String kobitonServerUrl = "https://svatitech:84b66fb4-d842-4faf-958d-66bd97a3118b@api.kobiton.com/wd/hub";
 	String deviceName = "Galaxy S10e";
 	String platformVersion = "9";
 	String platformName = "Android";
 	
 	@BeforeMethod
 	public void setUp() throws InterruptedException, MalformedURLException {
+		
+		DesiredCapabilities capabilities = new DesiredCapabilities();
+		// The generated session will be visible to you only. 
+		capabilities.setCapability("sessionName", "Automation test session");
+		capabilities.setCapability("sessionDescription", "");
+		capabilities.setCapability("deviceOrientation", "portrait");
+		capabilities.setCapability("captureScreenshots", true);
+		capabilities.setCapability("browserName", "chrome");
+		capabilities.setCapability("deviceGroup", "KOBITON");
+		// For deviceName, platformVersion Kobiton supports wildcard
+		// character *, with 3 formats: *text, text* and *text*
+		// If there is no *, Kobiton will match the exact text provided
+		capabilities.setCapability("deviceName", "Galaxy A7(2017)");
+		capabilities.setCapability("platformVersion", "7.0");
+		capabilities.setCapability("platformName", "Android");
+		driver = new AndroidDriver<WebElement>(new URL(kobitonServerUrl), capabilities);
+		/***
 		DesiredCapabilities capabilities = new DesiredCapabilities();
 		capabilities.setCapability("sessionName", "Automation test session");
 		capabilities.setCapability("sessionDescription", "");
@@ -34,18 +53,19 @@ public class AndroidBrowserTest {
 		capabilities.setCapability("deviceName", deviceName);
 		capabilities.setCapability("platformVersion", platformVersion);
 		capabilities.setCapability("platformName", platformName);
-
-		driver = new AndroidDriver<WebElement>(new URL(kobitonServerUrl), capabilities);
+		*/
+		
 
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		Thread.sleep(2000);
 		System.out.println("Device has been connected.");
+
 	}
 	
 	@Test
 	public void runTest() throws Exception{
 		System.out.println("Browser Opened");
-		driver.get("");
+		driver.get("http://google.com");
 		System.out.println("Navigated to the URL");
 		Thread.sleep(2000);
 	}
